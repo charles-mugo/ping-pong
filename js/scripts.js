55379c77f03d287caee48b058bbd3096c4dadca9
charles <adicharloh@gmail.com>
2018-05-07 08:48:44 +0300

    It will add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,20 @@ var countUp = function(countTo){
 
   return result;
 };
+
+
+
+
+//User Interface Logic
+$(document).ready(function(){
+  $("#userInput").submit(function(event){
+    $("#output").empty();
+    var countTo = parseInt($("input#countTo").val());
+    var output = countUp(countTo);
+
+    output.forEach(function(item){
+      $("#output").append('<li>' + item + '</li>');
+    });
+    event.preventDefault();
+  });
+});
