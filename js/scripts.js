5b254c61809cb787d969b8ba3d02693e2ef180f8
charles <adicharloh@gmail.com>
2018-05-07 08:47:36 +0300

    It will add a business logic

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//Business Logic
+var countUp = function(countTo){
+  var result = [];
+  for(var i = 1; i <= countTo; i++){
+      var index = result.indexOf(i);
+      if(i % 15 === 0) {
+        result.splice(index, 0);
+        result.push('pingpong');
+      }else if(i % 5 === 0) {
+        result.splice(index, 0);
+        result.push('pong');
+      }else if(i % 3 === 0) {
+        result.splice(index, 0);
+        result.push('ping');
+      }else{
+        result.push(i);
+      }
+  }
+
+  return result;
+};
